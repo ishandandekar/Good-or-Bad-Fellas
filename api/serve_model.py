9629978a772dc512af10c001d33869f3822805ec
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-07 23:43:02 +0530

    Debug model

--- api/serve_model.py
+++ api/serve_model.py
@@ -1,25 +1,61 @@
-import io
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
 import tensorflow as tf
-from pydantic import BaseModel
 import pickle
-from PIL import Image
 import numpy as np
+import string
+import re
+import os
+from pathlib import Path
+import uvicorn
 
-app = FastAPI()
-model = tf.keras.models.load_model("../models/model_0_conv1d.h5")
+app = FastAPI(
+    title="Good or Bad Fellas API",
+    description="A simple API that use NLP model to predict the sentiment of the movie's reviews",
+    version="69.420",
+)
+
+model = tf.keras.models.load_model("models/model_0_conv1d.h5")
+
+
+def standardization(text):
+    text = tf.strings.lower(text)
+    text = tf.strings.regex_replace(text, "<br />", " ")
+    return tf.strings.regex_replace(text, f"[{re.escape(string.punctuation)}]", "")
+
+
+MAX_FEATURES = 20000
+EMBED_DIM = 128
+SEQ_LENGTH = 500
+
+
 loaded_vectorizer = pickle.load(
-    open("../models/artifacts/model_0_conv1d_vectorization.pkl")
+    open("models/artifacts/model_0_conv1d_vectorization.pkl", "rb")
 )
-vectorizer = (
-    tf.keras.layers.TextVecorization()
-    .from_config(loaded_vectorizer["config"])
-    .set_weights(loaded_vectorizer["weights"])
-)
+print(type(loaded_vectorizer), end="")
+print("^^^^^^^^^^^^^^")
+print(loaded_vectorizer.keys(), end="")
+print("^^^^^^^^^^^^^^")
+print([type(value) for value in loaded_vectorizer.values()], end="")
+print("^^^^^^^^^^^^^^")
+vectorizer = tf.keras.layers.TextVectorization(
+    standardize=standardization,
+    max_tokens=MAX_FEATURES,
+    output_mode="int",
+    output_sequence_length=SEQ_LENGTH,
+).set_weights(loaded_vectorizer["weights"])
+print(type(vectorizer), end="")
+print("^^^^^^^^^^^^^^")
 
 
-class UserInput(BaseModel):
-    user_input: float
+def standardization(text):
+    text = tf.strings.lower(text)
+    text = tf.strings.regex_replace(text, "<br />", " ")
+    return tf.strings.regex_replace(text, f"[{re.escape(string.punctuation)}]", "")
+
+
+# def vectorize_text(text, vectorizer):
+#     # text = tf.expand_dims(text, -1)
+#     return vectorizer(text)
 
 
 @app.get("/")
@@ -29,15 +65,23 @@ async def index():
     }
 
 
-@app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
+@app.get("/predict-review/")
+async def predict(review: str):
     try:
-        contents = await file.read()
-        pil_image = Image.open(io.BytesIO(contents))
-        if pil_image.mode == "RGBA":
-            pil_image = pil_image.convert("RGB")
-    except:
-        raise HTTPException(status_code=404, detail=f"File is not an image")
+        text = tf.strings.lower(review)
+        text = tf.strings.regex_replace(text, "<br />", " ")
+        text = tf.strings.regex_replace(text, f"[{re.escape(string.punctuation)}]", "")
+        print(f"{type(text)} ???????")
+        print(f"{type(vectorizer)} #######")
+        text = vectorizer(text)
+        print(f"{type(text)} $$$$$$$$$")
+        prediction = model.predict([text])
+        print(prediction)
+        return {"Prediction": prediction}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404)
 
-    prediction = model.predict([UserInput.user_input])
-    return {"Prediction": tf.math.sigmoid(prediction)}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
