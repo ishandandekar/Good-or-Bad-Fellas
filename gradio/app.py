69d145389e28decf059231ecf0cc2b475d1c4ea5
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-08-09 10:13:28 +0530

    Add gradio for interface

---
+++ gradio/app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download("vader_lexicon")
+sid = SentimentIntensityAnalyzer()
+
+
+def sentiment_analysis(text):
+    scores = sid.polarity_scores(text)
+    del scores["compound"]
+    return scores
+
+
+app = gr.Interface(
+    fn=sentiment_analysis,
+    inputs=gr.Textbox(placeholder="Enter a positive or negative sentence here..."),
+    outputs="label",
+    interpretation="default",
+    examples=[["This is wonderful!"]],
+)
+
+app.launch()
